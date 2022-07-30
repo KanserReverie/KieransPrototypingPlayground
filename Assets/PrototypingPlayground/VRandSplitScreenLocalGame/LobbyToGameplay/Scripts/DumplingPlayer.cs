c6e20243dbbe5937ff62cbc0d5ddbdb1dce7539b
Kieran <kieranc.inbox@gmail.com>
2022-07-30 16:41:08 +1000

    Split screen and camera render over

---
+++ Assets/PrototypingPlayground/VRandSplitScreenLocalGame/LobbyToGameplay/Scripts/DumplingPlayer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace PrototypingPlayground.VRandSplitScreenLocalGame.LobbyToGameplay
+{
+    public class DumplingPlayer : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
