e5d35ee05e5377d85edf5c3f3b153484cfb70df3
Kieran <kieranc.inbox@gmail.com>
2022-08-03 17:17:26 +1000

    Folder change - Basic concepts and software patterns segregated

--- Assets/PrototypingPlayground/UsefulScripts/FollowGameObject.cs
+++ Assets/PrototypingPlayground/UsefulScripts/FollowGameObject.cs
@@ -6,13 +6,24 @@ namespace PrototypingPlayground.UsefulScripts
     {
         [SerializeField] private Transform transformToFollow;
         [SerializeField] private bool followRotation = false;
+        [SerializeField] private Vector3 offset;
         
         // Update is called once per frame
         void Update()
         {
-            this.transform.position = transformToFollow.position;
+            this.transform.position = transformToFollow.position + offset;
             if (followRotation)
                 this.transform.rotation = transformToFollow.rotation;
+        }
+        void OnValidate()
+        {
+            if (transformToFollow == null)
+                return;
+            transform.position = transformToFollow.position + offset;
+            if (followRotation)
+            {
+                transform.rotation = transformToFollow.rotation;
+            }
         }
     }
 }
