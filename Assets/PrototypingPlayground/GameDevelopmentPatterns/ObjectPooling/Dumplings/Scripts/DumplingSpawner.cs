29d48c57ae18e1cd7c19dfe971f65d8a70915e4e
Kieran <kieranc.inbox@gmail.com>
2022-08-07 14:11:16 +1000

    Spawning Dumplings to go between points

--- Assets/PrototypingPlayground/GameDevelopmentPatterns/ObjectPooling/Dumplings/Scripts/DumplingSpawner.cs
+++ Assets/PrototypingPlayground/GameDevelopmentPatterns/ObjectPooling/Dumplings/Scripts/DumplingSpawner.cs
@@ -1,32 +1,56 @@
+using UnityEditor;
 using UnityEngine;
-using UnityEngine.AI;
 using UnityEngine.Assertions;
 namespace PrototypingPlayground.GameDevelopmentPatterns.ObjectPooling.Dumplings
 {
     public class DumplingSpawner : MonoBehaviour
     {
         [SerializeField] private GameObject dumplingPrefab;
-        [SerializeField] private Transform spawnLocation;
-        [SerializeField] private Transform spawnDestination;
+        [SerializeField] private Rect dumplingSpawnPerimeter = new Rect(new Vector2(0,0),new Vector2(10,10));
+        private SpawnPerimeter spawnPerimeter;
+        
         private void Awake()
         {
             Assert.IsNotNull(dumplingPrefab.GetComponent<Dumpling>());
-            Assert.IsNotNull(spawnLocation);
-            Assert.IsNotNull(spawnDestination);
         }
         
         void Start()
         {
-            SpawnDumpling(spawnLocation);
+            spawnPerimeter = new SpawnPerimeter(dumplingSpawnPerimeter, this.transform);
+            Dumpling newDumpling = SpawnDumpling(spawnPerimeter.GetSpawnPointPosition(), spawnPerimeter.GetEndDestinationRotation());
+            newDumpling.NavMeshAgent.SetDestination(spawnPerimeter.GetEndDestinationPosition());
         }
-        private void SpawnDumpling(Transform _spawnLocation)
+        private void OnDrawGizmos()
         {
-                NavMeshHit navMeshHit;
-                NavMesh.SamplePosition(_spawnLocation.position, out navMeshHit, 2f, 0);
-                GameObject newDumpling = Instantiate(dumplingPrefab, _spawnLocation.position, _spawnLocation.rotation);
+            Gizmos.color = Color.yellow;
+            
+            // Get Center.
+            Vector3 startLocation = new Vector3(transform.position.x + dumplingSpawnPerimeter.x,transform.position.y, transform.position.z + dumplingSpawnPerimeter.y);
+            Gizmos.DrawSphere(startLocation, 0.3f);
+            
+            // Get all Corners
+            Vector3[] allCorners = new Vector3[4];
+
+            allCorners[0] = startLocation + new Vector3(dumplingSpawnPerimeter.width / 2, 0, dumplingSpawnPerimeter.height / 2);
+            allCorners[1] = startLocation + new Vector3(-dumplingSpawnPerimeter.width / 2, 0, dumplingSpawnPerimeter.height / 2);
+            allCorners[2] = startLocation + new Vector3(-dumplingSpawnPerimeter.width / 2, 0, -dumplingSpawnPerimeter.height / 2);
+            allCorners[3] = startLocation + new Vector3(dumplingSpawnPerimeter.width / 2, 0, -dumplingSpawnPerimeter.height / 2);
+            for (int i = 0; i < allCorners.Length; i++)
+            {
+                Gizmos.DrawSphere(allCorners[i], 0.2f);
+            }
+            int thickness = 4;
+            Handles.DrawBezier(allCorners[0],allCorners[1],allCorners[0],allCorners[1], Color.yellow,null,thickness);
+            Handles.DrawBezier(allCorners[1],allCorners[2],allCorners[1],allCorners[2], Color.yellow,null,thickness);
+            Handles.DrawBezier(allCorners[2],allCorners[3],allCorners[2],allCorners[3], Color.yellow,null,thickness);
+            Handles.DrawBezier(allCorners[3],allCorners[0],allCorners[3],allCorners[0], Color.yellow,null,thickness);
+        }
+        private Dumpling SpawnDumpling(Vector3 _spawnLocation, Quaternion _spawnRotation)
+        {
+                GameObject newDumpling = Instantiate(dumplingPrefab, _spawnLocation, _spawnRotation);
                 Dumpling dumpling = newDumpling.GetComponent<Dumpling>();
-                dumpling.NavMeshAgent.Warp(_spawnLocation.position);
-                dumpling.NavMeshAgent.SetDestination(spawnDestination.position);
+                dumpling.NavMeshAgent.Warp(_spawnLocation);
+                return dumpling;
         }
     }
 }
