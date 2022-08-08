e5f8eda8e3c102e9a62d9d65d38dc71e9991503a
Kieran <kieranc.inbox@gmail.com>
2022-08-08 12:15:08 +1000

    Quick fast float trigger output

---
+++ Assets/PrototypingPlayground/QuickPrototypes/TriggerTest/Scripts/DebugTriggerInputs.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PrototypingPlayground.QuickPrototypes.TriggerTest
+{
+    public class DebugTriggerInputs : MonoBehaviour
+    {
+        private float leftTriggerValue;
+        private float rightTriggerValue;
+
+        private void Update()
+        {
+            Debug.Log($"Left Trigger Value = {leftTriggerValue} || Right Trigger Value = {rightTriggerValue}");
+        }
+        public void OnLeftTriggerInput(InputAction.CallbackContext _leftTriggerInput)
+        {
+            leftTriggerValue = _leftTriggerInput.ReadValue<float>();
+        }
+        public void OnRightTriggerInput(InputAction.CallbackContext _rightTriggerInput)
+        {
+            rightTriggerValue = _rightTriggerInput.ReadValue<float>();
+        }
+    }
+}
