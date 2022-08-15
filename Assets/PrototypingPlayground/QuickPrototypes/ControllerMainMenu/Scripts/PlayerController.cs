cee3a8416d6b30f1369a907cb1fe3a6ae41b3e16
Kieran <kieranc.inbox@gmail.com>
2022-08-15 23:09:24 +1000

    Basic Menu Created

---
+++ Assets/PrototypingPlayground/QuickPrototypes/ControllerMainMenu/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+namespace PrototypingPlayground.QuickPrototypes.ControllerMainMenu
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private GameObject mainMenu;
+
+        public void OnPause(InputAction.CallbackContext _inputPressed)
+        {
+            if (_inputPressed.performed && Time.timeScale != 0)
+            {
+                mainMenu.gameObject.SetActive(true);
+            }
+        }
+}
+}
