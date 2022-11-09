f18a1906178291324fa6d7df330f193c64028466
Kieran <kieranc.inbox@gmail.com>
2022-11-09 17:40:40 +1100

    Added some basic functionality CommonlyUsedStaticMethods
    CommonlyUsedStaticMethods added:
    Basic "Open Next Scene"
    and "Load Scene by Name"

--- Assets/PrototypingPlayground/UsefulScripts/CommonlyUsedStaticMethods.cs
+++ Assets/PrototypingPlayground/UsefulScripts/CommonlyUsedStaticMethods.cs
@@ -25,5 +25,31 @@ namespace PrototypingPlayground.UsefulScripts
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+        
+        /// <summary>
+        /// This will load a scene based on a build index.
+        /// </summary>
+        /// <param name="buildIndex">Build index of the scene to load.</param>
+        public static void OpenSceneFromBuildIndex(int buildIndex)
+        {
+            SceneManager.LoadScene(buildIndex);
+        }
+        
+        /// <summary>
+        /// This will load the next scene in the build (based off scene index).
+        /// </summary>
+        public static void OpenNextSceneInBuild()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        
+        /// <summary>
+        /// This will load a scene based on a scene name in the build.
+        /// </summary>
+        /// <param name="sceneName">Name of the scene to load.</param>
+        public static void OpenSceneWithSceneName(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
     }
 }
