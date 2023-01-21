02087e97405f09c7e3bb3ed7f67b9489083b84ad
Kieran <kieranc.inbox@gmail.com>
2023-01-21 15:59:11 +1100

    Facade Read Me
    What Facade Is
    When to use
    Examples

---
+++ Assets/PrototypingPlayground/001GameDevelopmentPatterns/012FacadePattern/BikeEngine/Scenes/GameDevelopmentPatterns_FacadePattern_BikeEngine_BikeFacade.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace PrototypingPlayground._001GameDevelopmentPatterns._012FacadePattern.BikeEngine.Scenes
+{
+    public class GameDevelopmentPatterns_AdapterPattern_InventoryAsset_Saving : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
