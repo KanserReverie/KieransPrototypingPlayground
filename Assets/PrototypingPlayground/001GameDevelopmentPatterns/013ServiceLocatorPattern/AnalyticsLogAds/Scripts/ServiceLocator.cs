853092ec8c34794e0da23ef301dc66b7cd4b9191
Kieran <78832088+KanserReverie@users.noreply.github.com>
2023-02-06 15:32:03 +1100

    Service Locator Started For Global Services Example

---
+++ Assets/PrototypingPlayground/001GameDevelopmentPatterns/013ServiceLocatorPattern/AnalyticsLogAds/Scripts/ServiceLocator.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+namespace PrototypingPlayground._001GameDevelopmentPatterns._013ServiceLocatorPattern.AnalyticsLogAds.Scripts
+{
+    /// <summary>
+    /// This is going to be our "White Pages" for this project.
+    /// </summary>
+    public static class ServiceLocator
+    {
+        /// <summary> All services to be globally accessible </summary>
+        private static readonly IDictionary<Type, object> Services = new Dictionary<Type, Object>();
+
+        public static void RegisterService<T>(T service)
+        {
+            if (!Services.ContainsKey(typeof(T)))
+            {
+                Services[typeof(T)] = service;
+            }
+            else
+            {
+                throw new
+                    ApplicationException
+                    ("Service already registered");
+            }
+        }
+
+        public static T GetService<T>()
+        {
+            try
+            {
+                return (T) Services[typeof(T)];
+            }
+            catch
+            {
+                throw new
+                    ApplicationException
+                    ("Requested service not found.");
+            }
+        }
+    }
+}
