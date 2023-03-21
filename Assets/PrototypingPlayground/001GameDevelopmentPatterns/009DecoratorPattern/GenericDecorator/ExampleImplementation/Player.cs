bd5703627b37f0684975efa1476acae981ae3d0b
Kieran <78832088+KanserReverie@users.noreply.github.com>
2023-03-22 10:18:16 +1100

    Slowly Understanding the Decorator Pattern again

---
+++ Assets/PrototypingPlayground/001GameDevelopmentPatterns/009DecoratorPattern/GenericDecorator/ExampleImplementation/Player.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace PrototypingPlayground._001GameDevelopmentPatterns._009DecoratorPattern.GenericDecorator.ExampleImplementation
+{
+    public class Player : MonoBehaviour
+    {
+        
+    }
+}
