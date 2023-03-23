8f7c19893359a0ad9e17525842df629024d8a33d
Kieran <78832088+KanserReverie@users.noreply.github.com>
2023-03-24 10:42:17 +1100

    Decorator Example

--- Assets/PrototypingPlayground/001GameDevelopmentPatterns/009DecoratorPattern/GenericDecorator/ExampleImplementation/DecoratorScripts/PlayerDecoratorControllerBehaviour.cs
+++ Assets/PrototypingPlayground/001GameDevelopmentPatterns/009DecoratorPattern/GenericDecorator/ExampleImplementation/DecoratorScripts/PlayerDecoratorControllerBehaviour.cs
@@ -22,10 +22,10 @@ namespace PrototypingPlayground._001GameDevelopmentPatterns._009DecoratorPattern
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Alpha1)) { Debug.Log("Add Decorator 1"); newPlayer.playerDecorators.Add(decorator1); }
-            if (Input.GetKeyDown(KeyCode.Alpha2)) { Debug.Log("Add Decorator 2"); newPlayer.playerDecorators.Add(decorator2); }
-            if (Input.GetKeyDown(KeyCode.Alpha3)) { Debug.Log("Add Decorator 3"); newPlayer.playerDecorators.Add(decorator3); }
-            if (Input.GetKeyDown(KeyCode.R)) { Debug.Log("Remove all decorators"); newPlayer.playerDecorators = new List<IPlayerDecoratableVariables>(); }
+            if (Input.GetKeyDown(KeyCode.Alpha1)) { Debug.Log($"Add Decorator 1 - {decorator1.name}"); newPlayer.playerDecorators.Add(decorator1); }
+            if (Input.GetKeyDown(KeyCode.Alpha2)) { Debug.Log($"Add Decorator 2 - {decorator2.name}"); newPlayer.playerDecorators.Add(decorator2); }
+            if (Input.GetKeyDown(KeyCode.Alpha3)) { Debug.Log($"Add Decorator 3 - {decorator3.name}"); newPlayer.playerDecorators.Add(decorator3); }
+            if (Input.GetKeyDown(KeyCode.R)) { Debug.Log("Remove All Decorators"); newPlayer.playerDecorators = new List<IPlayerDecoratableVariables>(); }
         }
     }
 }
