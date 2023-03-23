b446661b85e4e576f91295fcf93b806e8ee2b538
Kieran <78832088+KanserReverie@users.noreply.github.com>
2023-03-23 19:26:22 +1100

    Changing the scene names and player for the Decorator Pattern Example

--- Assets/PrototypingPlayground/001GameDevelopmentPatterns/009DecoratorPattern/GenericDecorator/ExampleImplementation/Player.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace PrototypingPlayground._001GameDevelopmentPatterns._009DecoratorPattern.GenericDecorator.ExampleImplementation
-{
-    public class Player : MonoBehaviour
-    {
-        
-    }
-}
