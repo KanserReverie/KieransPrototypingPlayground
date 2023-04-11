e3b79453216df6e7b98518b71d4d9728a60a44f4
Kieran <78832088+KanserReverie@users.noreply.github.com>
2023-04-11 10:38:45 +1000

    Changing Generic Solution - Spatial Partition

--- Assets/PrototypingPlayground/001GameDevelopmentPatterns/010SpatialPartition/GenericSpatialPartition/LayerOnePartitionObject.cs
+++ Assets/PrototypingPlayground/001GameDevelopmentPatterns/010SpatialPartition/GenericSpatialPartition/LayerOnePartitionObject.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace PrototypingPlayground._001GameDevelopmentPatterns._010SpatialPartition.GenericSpatialPartition
 {
-    public class PartitionGrid : MonoBehaviour
+    public class LayerOnePartitionObject : MonoBehaviour
     {
         
     }
