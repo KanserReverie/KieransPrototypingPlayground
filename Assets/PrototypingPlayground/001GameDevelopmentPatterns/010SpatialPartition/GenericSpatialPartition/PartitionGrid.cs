ea715c90cc545e07979e37d48ce4d05aaf585581
Kieran <78832088+KanserReverie@users.noreply.github.com>
2023-04-19 09:31:52 +1000

    Admitting Making a Generic Partition is Too Hard

--- Assets/PrototypingPlayground/001GameDevelopmentPatterns/010SpatialPartition/GenericSpatialPartition/PartitionGrid.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace PrototypingPlayground._001GameDevelopmentPatterns._010SpatialPartition.GenericSpatialPartition
-{
-    public class PartitionGrid : MonoBehaviour
-    {
-        // A grid of partition objects, single dimensions or grids.
-    }
-}
