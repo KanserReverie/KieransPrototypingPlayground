a7a886610084ccd7a20515781700757373ecc286
Kieran <78832088+KanserReverie@users.noreply.github.com>
2023-04-28 14:28:29 +1000

    Example Partition Solution - Spawning Grids of Grids of Enemy Cubes

--- Assets/PrototypingPlayground/001GameDevelopmentPatterns/010SpatialPartition/GenericGridPartition/ExampleImplementation/Scripts/GridEnemyCubes.cs
+++ Assets/PrototypingPlayground/001GameDevelopmentPatterns/010SpatialPartition/GenericGridPartition/ExampleImplementation/Scripts/GridEnemyCubes.cs
@@ -2,6 +2,8 @@ namespace PrototypingPlayground._001GameDevelopmentPatterns._010SpatialPartition
 {
     /// <summary>
     /// This will spawn a grid of enemy cubes.
+    ///
+    /// We will use it to spawn grids of grids.
     /// </summary>
     public class GridEnemyCubes : PartitionGridBehaviour<EnemyCube>
     {
