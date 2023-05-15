ac7c5039e46555938565a3649b66cd5807239b63
Kieran <78832088+KanserReverie@users.noreply.github.com>
2023-05-16 09:41:26 +1000

    Started Dino's pattern

---
+++ Assets/PrototypingPlayground/001GameDevelopmentPatterns/013ServiceLocatorPattern/GenericServiceLocatorPattern/IGameService.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace PrototypingPlayground._001GameDevelopmentPatterns._013ServiceLocatorPattern.GenericServiceLocatorPattern
+{
+    /// <summary>
+    /// Base interface for our service locator to work with.
+    /// Services implementing this interface will be retrievable using the locator.
+    /// </summary> 
+    public interface IGameService
+    {
+    }
+}
