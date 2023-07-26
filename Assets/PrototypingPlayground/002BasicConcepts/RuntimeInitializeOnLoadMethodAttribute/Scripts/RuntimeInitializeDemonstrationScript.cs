cd5553a7efed6d694cd57856649d3456b1b116a1
Kieran <78832088+KanserReverie@users.noreply.github.com>
2023-07-26 15:44:32 +1000

    Added Comment for previous runtime scripts

--- Assets/PrototypingPlayground/002BasicConcepts/RuntimeInitializeOnLoadMethodAttribute/Scripts/RuntimeInitializeDemonstrationScript.cs
+++ Assets/PrototypingPlayground/002BasicConcepts/RuntimeInitializeOnLoadMethodAttribute/Scripts/RuntimeInitializeDemonstrationScript.cs
@@ -8,21 +8,36 @@ namespace PrototypingPlayground._002BasicConcepts.RuntimeInitializeOnLoadMethodA
         {
             Debug.Log("---- Start() - BEGIN ----");
             Debug.Log("[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.\"xx\")] runs EVERY time the game is RUN or BUILT either:");
-            
             Debug.Log("Before Scene AWAKE functions called - \"xx\"=BeforeSceneLoad");
             Debug.Log("After Scene AWAKE functions called - \"xx\"=AfterSceneLoad");
             Debug.Log("Callback when all assemblies loaded assets initialized - \"xx\"=AfterAssembliesLoaded");
             Debug.Log("Immediately before the splash screen is shown - \"xx\"=BeforeSplashScreen");
             Debug.Log("Callback used for registration of subsystems - \"xx\"=SubsystemRegistration");
             Debug.Log("------------------");
-            
-            Debug.Log("See more here\nhttps://docs.unity3d.com/ScriptReference/RuntimeInitializeLoadType.html");
+
+            Debug.Log("See more here ->https://docs.unity3d.com/ScriptReference/RuntimeInitializeLoadType.html");
             Debug.Log("---- Start() - END ----");
-            
+
             Debug.Log("CODE IS COMMENTED OUT OTHERWISE IT WILL RUN ON EVERY SCENE");
             Debug.Log("REMOVE COMMENTS TO SEE IT IN ACTION");
         }
-        
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!! DISCLAIMER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //
+        // !!!!!!!!!!!!!!!!!------->     Uncomment all the code below to see it in action.!!!!!!!!!!!!!!!!!!!!!
+        //
+        //
+        // I have it commented so this doesn't run EVERY SCENE
+        // USES :
+        // ||Service locator pattern|| to make sure references are made OR 
+        // ||Instantiating managers|| loaded from resources :D
+        //
+        //
+        // ALSO TO NOTE THERE ARE METHODS FOR RUNNING CODE IN EDITOR BEFORE OTHER STUFF RUNS :)
+        // See the package particle effects and the read me always opens first.
+        //
+        //
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// 
         // [UnityEngine.RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         // static void OnBeforeSceneLoadRuntimeMethod()
         // {
