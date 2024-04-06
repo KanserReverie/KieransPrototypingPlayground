7367f04829fc2326d691dd504c035d73761fd68f
Kieran <78832088+KanserReverie@users.noreply.github.com>
2024-04-06 15:30:20 +1100

    Character controller + Movement

--- Assets/PrototypingPlayground/003ProjectPrototypes/CodeCampIgnitePlatformer/Scripts/PlayerMovement.cs
+++ Assets/PrototypingPlayground/003ProjectPrototypes/CodeCampIgnitePlatformer/Scripts/PlayerMovement.cs
@@ -6,22 +6,42 @@ namespace PrototypingPlayground._003ProjectPrototypes.CodeCampIgnitePlatformer.S
     public class PlayerMovement : MonoBehaviour
     {
         private Animator thisAnimator;
+        private CharacterController thisCharacterController;
+        public float jumpForce = 650;
+        public float movementSpeed = 450;
+        public float gravity = 1200;
+        private Vector3 movement;
 
         private void Start()
         {
             thisAnimator = GetComponent<Animator>();
+            thisCharacterController = GetComponent<CharacterController>();
+            movement = Vector3.zero;
         }
 
-        void Update()
+        private void FixedUpdate()
         {
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            if (!thisCharacterController.isGrounded)
             {
-                thisAnimator.Play("Hero Left Animation");
+                movement += Vector3.down * gravity * Time.fixedDeltaTime;
             }
-            if (Input.GetKeyDown(KeyCode.RightArrow))
+            
+            if (Input.GetKey(KeyCode.RightArrow))
             {
                 thisAnimator.Play("Hero Right Animation");
+                movement += Vector3.right * movementSpeed * Time.fixedDeltaTime;
             }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                thisAnimator.Play("Hero Left Animation");
+                movement += Vector3.left * movementSpeed * Time.fixedDeltaTime;
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                movement = new Vector3(movement.x, jumpForce, movement.z);
+            }
+            thisCharacterController.Move(movement);
         }
     }
 }
