3e23674e749ab4f7ec706000cc35da281501f8dd
Kieran <78832088+KanserReverie@users.noreply.github.com>
2024-05-11 19:45:48 +1000

    Int Input Field to UI - Restrict to Int

---
+++ Assets/PrototypingPlayground/002BasicConcepts/InputFields/Scripts/LinkInputFieldToVariable.cs
@@ -0,0 +1,32 @@
+using Palmmedia.ReportGenerator.Core.Common;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PrototypingPlayground._002BasicConcepts.InputFields
+{
+    public class LinkInputFieldToVariable : MonoBehaviour
+    {
+        public TMP_InputField inputFieldToModify;
+        public string changedString;
+        public int changedInt;
+        
+        void Start()
+        {
+            inputFieldToModify.contentType = TMP_InputField.ContentType.IntegerNumber;
+        }
+        
+        void Update()
+        {
+            changedString = inputFieldToModify.text;
+            if (changedString != "")
+            {
+                changedInt = changedString.ParseLargeInteger();
+            }
+            else
+            {
+                changedInt = 0;
+            }
+        }
+    }
+}
